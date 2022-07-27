4716a5857f28f62507293a319178390ceb66f8da
choiyuna <choiyuna0209@gmail.com>
2022-07-27 19:45:04 +0900

    Controller V3
    ModelView 객체 추가

---
+++ Inflearn-Spring/spring_mvc/1/servlet/src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberFormControllerV3.java
@@ -0,0 +1,13 @@
+package hello.servlet.web.frontcontroller.v3.controller;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+
+import java.util.Map;
+
+public class MemberFormControllerV3 implements ControllerV3 {
+    @Override
+    public ModelView process(Map<String, String> paramMap) {
+        return new ModelView("new-form"); // 이 생성자를 쓰면 아직 model 객체는 null인거지?
+    }
+}
